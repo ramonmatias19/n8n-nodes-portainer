f62801e12437ac1017eca0ae2724c2ae7fc77972
raphael.zhang <albuz.zly@gmail.com>
2026-05-27 19:16:18 +0800

    fix: expression syntax error in portainer url

--- credentials/PortainerApi.credentials.ts
+++ credentials/PortainerApi.credentials.ts
@@ -53,7 +53,7 @@ export class PortainerApi implements ICredentialType {
 	test: ICredentialTestRequest = {
 		request: {
 			method: 'GET',
-			url: '={{$credentials.baseUrl.replace(/\/+$/, "")}}/api/users/me',
+			url: '={{$credentials.baseUrl.replace(new RegExp("/+$"), "")}}/api/users/me',
 			skipSslCertificateValidation: '={{$credentials.ignoreSsl}}',
 		},
 		rules: [
